0ac394bc32a06803bbc92e9b03cc316cbe471016
sebLopezCot <sclopez2@hotmail.com>
2017-01-25 12:56:04 -0500

    Changed location of ranking tables for eclipse convenience

--- src/pokerbots/spam_framework/ranking_tables/RankingTableConfig.java
+++ src/pokerbots/spam_framework/ranking_tables/RankingTableConfig.java
@@ -2,11 +2,11 @@ package pokerbots.spam_framework.ranking_tables;
 
 public class RankingTableConfig {
 
-	public static final String FLOP_RANKINGS_LOCATION 		= "ranking_tables/flop_rankings/";
-	public static final String FLOP_DECISION_RULE_LOCATION 	= "ranking_tables/flop_before_discard_decision_rule/";
-	public static final String TURN_RANKINGS_LOCATION 		= "ranking_tables/turn_rankings/";
-	public static final String TURN_DECISION_RULE_LOCATION 	= "ranking_tables/turn_before_discard_decision_rule/";
-	public static final String RIVER_RANKINGS_LOCATION 		= "ranking_tables/river_rankings/";
-	public static final String PREFLOP_RANKINGS_LOCATION	= "ranking_tables/";
+	public static final String FLOP_RANKINGS_LOCATION 		= "../ranking_tables/flop_rankings/";
+	public static final String FLOP_DECISION_RULE_LOCATION 	= "../ranking_tables/flop_before_discard_decision_rule/";
+	public static final String TURN_RANKINGS_LOCATION 		= "../ranking_tables/turn_rankings/";
+	public static final String TURN_DECISION_RULE_LOCATION 	= "../ranking_tables/turn_before_discard_decision_rule/";
+	public static final String RIVER_RANKINGS_LOCATION 		= "../ranking_tables/river_rankings/";
+	public static final String PREFLOP_RANKINGS_LOCATION	= "../ranking_tables/";
 	public static final String PREFLOP_FILE					= "preflop";
 }
